fd01444e7f65e57598e1a8c0b5beacf0f983f4f3
Filippo <tomainofilippo@gmail.com>
2024-08-18 10:40:21 +0200

    Finished the javascript part
    I could not find another way than the nested ifs
    and writing all the possibilities between the
    computer choices and the player one.

--- script.js
+++ script.js
@@ -17,18 +17,72 @@ function play() {
     // getting the value of the radio input selected
     for(let i = 0; i < radioElement.length; i++) {
         if(radioElement[i].checked) {
-            radioValue = radioElement[i].value + " "
-            console.log(radioValue)
+            radioValue = radioElement[i].value
         }
     }
 
-
+    //  arrayOption is needed for the computer to choose an option with a random number from 1 to 3
     let arrayOption = ["rock", "paper", "scissors"]
-    let random = Math.floor(Math.random() * 4)
+    let random = Math.floor(Math.random() * 3)
 
-    for(let i = 0; i < arrayOption.length; i++) {
-        if(random == arrayOption[i]) {
-            
-        }
+    const computerChoice = document.getElementById("computer-choice")
+    const output = document.getElementById("output")
+    
+
+    if(arrayOption[random] == radioValue) {
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "It's a tie!"
+        console.log("tie")
+
+    } else if(arrayOption[random] == "rock" && radioValue == "paper") {
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "You Won"
+        console.log("You Won!")
+
+    } else if(arrayOption[random] == "scissors" && radioValue == "paper") {
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "You Lost"
+        console.log("You Lost")
+
+    } else if(arrayOption[random] == "paper" && radioValue == "scissors") {
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "You won"
+        console.log("You Won")
+
+    } else if(arrayOption[random] == "paper" && radioValue == "rock"){
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "You Lost"
+        console.log("You Lost")
+
+    } else if(arrayOption[random] == "scissors" && radioValue == "rock") {
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "You Won"
+        console.log("You Won")
+
+    } else if(arrayOption[random] == "rock" && radioValue == "scissors"){
+        computerChoice.style.display = "block"
+        output.style.display = "block"
+        computerChoice.innerHTML = "The computer chose " + arrayOption[random]
+        output.innerHTML = "You Lost"
+        console.log("You Lost")
+
+    } else {
+        output.style.display = "block"
+        output.innerHTML = "You have not chosen an option, reload the page to play."
+        console.log("You have not chosen an option, reload the page to play.")
     }
+
+
 }  
